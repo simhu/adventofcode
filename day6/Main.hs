3e5c6a356dff708a653d1820098cc525e3d1e6b4
Simon Huber <simonh@fripost.org>
2021-12-09 13:51:52 +0100

    Day 6 solutions

---
+++ day6/Main.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+import Text.Parsec
+import Text.Parsec.String (Parser, parseFromFile)
+import qualified Data.Map.Strict as M
+
+newtype Pool = Pool { unPool :: [Int] }
+  deriving (Show, Eq)
+
+sea :: Parser [Int]
+sea = num `sepBy` char ','
+  where num = read <$> many1 digit
+
+fromSea :: [Int] -> Pool
+fromSea ns = Pool $ map (\n -> length (filter (==n) ns)) [0..8]
+
+evolve :: Pool -> Pool
+evolve (Pool fish) = Pool $ xs ++ ((nextGen+y):ys) ++ [nextGen]
+  where nextGen    = head fish
+        (xs, y:ys) = splitAt 6 (tail fish)
+
+alive :: Pool -> Int
+alive = sum . unPool
+
+main :: IO ()
+main = do
+  Right s <- parseFromFile sea "input"
+  putStrLn "PART 1"
+  putStrLn (show (alive ((iterate evolve (fromSea s)) !! 80)))
+
+  putStrLn "PART 2"
+  putStrLn (show (alive ((iterate evolve (fromSea s)) !! 256)))
+
+
