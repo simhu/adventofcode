6a02c4b786a30490fea364fec3b12fc3278a1fe7
Simon Huber <simonh@fripost.org>
2021-12-09 03:52:12 +0100

    Day 4 solutions

---
+++ day4/Main.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+import Text.Parsec hiding (State)
+import Text.Parsec.String (Parser, parseFromFile)
+import Data.List (findIndex, transpose)
+import Data.Maybe (mapMaybe, listToMaybe)
+
+-- TODO: cleanup?
+
+data Cell = Marked Int | Unmarked Int
+  deriving (Eq, Show)
+
+type Board = [[Cell]]
+type Move = Int
+
+num :: Parser Int
+num = read <$> many1 digit
+
+cell :: Parser Cell
+cell = Unmarked <$> num
+
+board :: Parser Board
+-- NB: Can't use 'spaces' instead of the 'many (char ' ')' as '\n'
+-- also parsed by spaces..
+board = many1 (many (char ' ') >> cell) `endBy1` endOfLine
+
+isMarked :: Cell -> Bool
+isMarked (Marked _) = True
+isMarked _          = False
+
+isWinner :: Board -> Bool
+isWinner b = any (all isMarked) b || any (all isMarked) (transpose b)
+
+mark :: Move -> Board -> Board
+mark m = map (map (\case { Unmarked x | x == m -> Marked x; z -> z }))
+
+-- a bit ugly, but oh..
+getMarked :: [[Board]] -> Maybe (Int, Board)
+getMarked bss = do i <- findIndex (any isWinner) bss
+                   let bs = bss !! i
+                   j <- findIndex isWinner bs
+                   return (i, bs !! j)
+
+-- even uglier?
+getLooser :: [[Board]] -> Maybe (Int, Board)
+getLooser bss =
+  do i <- findIndex (\bs -> length (filter (not . isWinner) bs) == 1) bss
+     let bs = bss !! i
+     j <- findIndex (not . isWinner) bs
+     let bssj = map (!! j) bss
+     k <- findIndex isWinner bssj
+     return (k, bssj !! k)
+
+main :: IO ()
+main = do
+  let fparser = (,) <$> (num `sepBy1` (char ',')) <* endOfLine <* endOfLine
+                    <*> board `sepEndBy1` endOfLine
+  parseFromFile fparser "input" >>= \case
+    Left err -> putStrLn (show err) >> return ()
+    Right (moves, boards) -> do
+      let bss = scanl (flip $ map . mark) boards moves
+      putStrLn "PART 1"
+      case getMarked bss of
+        Nothing -> error "No game won!"
+        Just (i,b)  -> do
+          let last = moves !! (i - 1)  -- the scanl also contains boards (no move)
+              usum = sum (concat (map (map (\case { Unmarked n -> n; _ -> 0 })) b))
+          putStrLn ("Last move: " ++ show last)
+          putStrLn ("Sum of unmarked elements on winner board: " ++ show usum)
+          putStrLn ("Product: " ++ show (last * usum))
+      putStrLn "PART 2"
+      case getLooser bss of
+        Nothing -> error "No clear looser!"
+        Just (k,b)  -> do
+          let last = moves !! (k - 1)
+              usum = sum (concat (map (map (\case { Unmarked n -> n; _ -> 0 })) b))
+          putStrLn ("Last looser move: " ++ show last)
+          putStrLn ("Sum of unmarked elements on looser board: " ++ show usum)
+          putStrLn ("Product: " ++ show (last * usum))
+
+
