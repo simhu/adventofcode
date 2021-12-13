b2bd17f83bac0b1248106347c63ea3ff14d8dc66
Simon Huber <simonh@fripost.org>
2021-12-14 00:29:03 +0100

    Day 11 and 12

---
+++ day11/Main.hs
@@ -0,0 +1,46 @@
+module Main where
+
+import Text.Parsec
+import Text.Parsec.String (Parser, parseFromFile)
+import Data.Monoid
+import Control.Monad.Writer
+import Control.Monad (foldM)
+import Data.List (delete, findIndex)
+import qualified Data.Map.Strict as M
+
+type Loc = (Int,Int)
+type Grid = M.Map Loc Int
+
+annotate :: [[a]] -> [(Loc,a)]
+annotate zss = [ ((x,y),z) | (y,zs) <- zip [0..] zss, (x,z) <- zip [0..] zs ]
+
+grid :: Parser Grid
+grid = (M.fromList . annotate) <$>
+       many (read . singleton <$> digit) `endBy` newline
+  where singleton x = [x]
+
+-- flash :: Grid -> PGrid -> PGrid
+adj :: Loc -> [Loc]
+adj (x,y) = delete (x,y) [ (x+dx,y+dy) | dx <- [-1,0,1], dy <- [-1,0,1] ]
+
+step :: Grid -> Writer (Sum Int) Grid
+step g = go (M.map (+1) g) M.empty
+  where go :: Grid -> Grid -> Writer (Sum Int) Grid
+        go g flashed = let fl = M.filter (> 9) (M.difference g flashed) in
+          if M.null fl then return $ M.unionWith (const (const 0)) g flashed
+          else do
+            let inc = M.foldMapWithKey (const . adj) fl
+                g' = foldr (M.adjust (+1)) g inc
+            tell (Sum $ M.size fl)
+            go g' (flashed `M.union` fl)
+
+main :: IO ()
+main = do
+  Right g <- parseFromFile grid "input"
+  -- Part 1
+  let it100 = foldM (const . step) g [0..99]
+  putStrLn (show (getSum (execWriter it100)))
+  -- Part 2
+  let iter = iterate (fst . runWriter . step) g
+  let Just n = findIndex (all ((==0) . snd) . M.assocs) iter
+  putStrLn (show n)
---
+++ day12/Main.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+import Text.Parsec
+import Text.Parsec.String (Parser, parseFromFile)
+import Data.List ((\\))
+import Data.Maybe (isJust,maybeToList)
+import qualified Data.Map as M
+import Data.Map ((!))
+
+data Vertex = Start | End | Small String | Large String
+  deriving (Eq,Ord)
+
+instance Show Vertex where
+  show Start     = "start"
+  show End       = "end"
+  show (Small s) = s
+  show (Large s) = s
+
+type Graph = M.Map Vertex [Vertex]
+
+toGraph :: [(Vertex, Vertex)] -> Graph
+toGraph [] = M.empty
+toGraph ((f,t):es) = M.insertWith (++) f [t] $ M.insertWith (++) t [f] $ toGraph es
+
+graph :: Parser Graph
+graph = toGraph <$> edge `sepEndBy1` newline
+  where vertex = string "start" *> return Start <|> string "end" *> return End <|>
+          Small <$> many1 (oneOf ['a'..'z']) <|> Large <$> many1 (oneOf ['A'..'Z'])
+        edge = (,) <$> vertex <*> (char '-' *> vertex)
+
+isLarge :: Vertex -> Bool
+isLarge (Large _) = True
+isLarge _         = False
+
+allPaths :: Graph -> Vertex -> Vertex -> [[Vertex]]
+allPaths g s e = go [] [] s
+  where
+    go acc smallvisited v
+      | v == e    = [reverse (e:acc)]
+      | null suc  = []
+      | isLarge v = concatMap (go (v:acc) smallvisited) suc
+      | otherwise = concatMap (go (v:acc) (v:smallvisited)) suc
+      where suc = g ! v \\ smallvisited
+
+small :: [Vertex] -> [Vertex]
+small = filter (\case { Small _ -> True; _ -> False })
+
+allPaths2 :: Graph -> Vertex -> Vertex -> [[Vertex]]
+allPaths2 g s e = concatMap (\v -> go [] [] v s) (Nothing:map Just (small $ M.keys g))
+  where
+    go acc smallvisited twice v
+      | Just v' <- twice, length (filter (==v') smallvisited) > 2 = []
+      | v == e    = case twice of
+                      Just v' | length (filter (==v') smallvisited) < 2 -> []
+                      _ -> [reverse (e:acc)]
+      | null suc  = []
+      | isLarge v = concatMap (go (v:acc) smallvisited twice) suc
+      | otherwise = concatMap (go (v:acc) (v:smallvisited) twice) suc
+      where suc = g ! v \\ (smallvisited \\ maybeToList twice)
+
+main :: IO ()
+main = do
+  Right es <- parseFromFile graph "input"
+  -- putStrLn (show es)
+  let ap = allPaths es Start End
+
+  putStrLn (show (length ap))
+  let ap2 = allPaths2 es Start End
+  -- putStrLn (unlines (map show ap2))
+  putStrLn (show (length ap2))
+
