809fed9ef19fed4c8422868f034a3de1346cab58
Simon Huber <simonh@fripost.org>
2021-12-14 15:58:01 +0100

    Day 14 solutions

---
+++ day14/Main.hs
@@ -0,0 +1,40 @@
+module Main where
+
+import Text.Parsec
+import Text.Parsec.String (Parser, parseFromFile)
+import Data.Map.Strict (Map, (!))
+import qualified Data.Map.Strict as M
+import Data.List (partition, sort)
+
+type Template = String
+type Rules = [((Char, Char), Char)]
+type Counts = Map (Char, Char) Int
+
+rules :: Parser Rules
+rules = ((,) <$> ((,) <$> letter <*> letter) <*> (string " -> " *> letter))
+        `sepEndBy` endOfLine
+
+countList :: Eq a => [a] -> [(a,Int)]
+countList []     = []
+countList (x:xs) = (x, length eqx + 1):(countList neqx)
+ where (eqx,neqx) = partition (==x) xs
+
+toCounts :: Template -> Counts
+toCounts xs = M.fromList $ countList $ zip xs (tail xs ++ [' '])
+
+step :: Rules -> Counts -> Counts
+step rules cs = M.unionsWith (+) (cs:add)
+  where add = [ M.fromListWith (+) [((l,r),-n), ((l,c),n), ((c,r), n) ]
+              | ((l,r), c) <- rules, (l,r) `M.member` cs , let n = cs ! (l,r) ]
+
+counts :: Counts -> [(Char,Int)]
+counts = M.toList . M.mapKeysWith (+) fst
+
+main :: IO ()
+main = do
+  let p = (,) <$> (many letter <* count 2 endOfLine) <*> rules
+  Right (t,rs) <- parseFromFile p "input"
+  let answ x = last ns - head ns
+        where ns = sort $ map snd $ counts $ iterate (step rs) (toCounts t) !! x
+  putStrLn (show $ answ 10)
+  putStrLn (show $ answ 40)
